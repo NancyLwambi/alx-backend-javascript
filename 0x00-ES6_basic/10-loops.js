409f69e295608917da2e0072fa2866819710f4c9
NancyLwambi <nancylwambi@gmail.com>
2022-12-19 04:12:40 -0800

    loops

--- 0x00-ES6_basic/10-loops.js
+++
@@ -1,8 +0,0 @@
-export default function appendToEachArrayValue(array, appendString) {
-  for (const idx of array) {
-    const value = array[idx];
-    array[idx] = appendString + value;
-  }
-
-  return array;
-}
