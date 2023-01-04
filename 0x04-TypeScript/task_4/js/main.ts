10e26046c0e76133579102bae51238db29dcd85b
NancyLwambi <nancylwambi.com>
2023-01-04 05:47:36 -0800

    0x04. Typescript

---
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -0,0 +1,19 @@
+/// <reference path="./subjects/Cpp.ts" />
+/// <reference path="./subjects/Java.ts" />
+/// <reference path="./subjects/React.ts" />
+/// <reference path="./subjects/Subject.ts" />
+/// <reference path="./subjects/Teacher.ts" />
+// @ts-ignore
+
+namespace Subjects {
+    export const cpp = Cpp;
+    export const java = Java;
+    export const react = React;
+    // @ts-ignore
+    export const cTeacher: Teacher
+    {
+        "test";
+        "test";
+        10
+    }
+}
